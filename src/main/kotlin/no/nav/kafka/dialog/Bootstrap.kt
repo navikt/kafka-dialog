f07dbf71ba0e3b5f1d46ec83b51868924db6fcec
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2022-11-23 10:27:03 +0100

    Deploy dev sf-stilling again

--- src/main/kotlin/no/nav/kafka/dialog/Bootstrap.kt
+++ src/main/kotlin/no/nav/kafka/dialog/Bootstrap.kt
@@ -5,7 +5,7 @@ import no.nav.syfo.dialogmote.avro.KDialogmoteStatusEndring
 
 val application = when (env(env_DEPLOY_APP)) {
     "sf-dialogmote" -> KafkaPosterApplication<String, KDialogmoteStatusEndring>(envAsSettings(env_POSTER_SETTINGS))
-    "sf-stilling" -> KafkaPosterApplication<String, Ad>(envAsSettings(env_POSTER_SETTINGS), ::truncateAdtext)
+    "sf-stilling" -> KafkaPosterApplication<String, Ad>(envAsSettings(env_POSTER_SETTINGS), ::truncateAdtext).also { "Instansiating sf-stilling application with Ad" }
     else -> KafkaPosterApplication<String, String>(envAsSettings(env_POSTER_SETTINGS))
 }
 
