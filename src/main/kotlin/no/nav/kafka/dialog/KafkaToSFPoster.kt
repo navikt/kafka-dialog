990c783282dd4f6f47ae725eca30423e711427f3
Kenneth Sørensen <kenneth.sorensen@nav.no>
2022-11-24 12:04:24 +0100

    Update KafkaToSFPoster.kt

--- src/main/kotlin/no/nav/kafka/dialog/KafkaToSFPoster.kt
+++ src/main/kotlin/no/nav/kafka/dialog/KafkaToSFPoster.kt
@@ -89,7 +89,7 @@ class KafkaToSFPoster<K, V>(val settings: List<Settings> = listOf(), val modifie
                                 log.info { "Special case bytes Avro - SAMPLE - made a sample" }
                             } else {
                                 log.info { "Samples" }
-                                log.info { "KEY: ${it.key()}" }
+                                log.info { "VALUE: ${it.value()}" }
                                 File("/tmp/samples").appendText("KEY: ${it.key()}\nVALUE: ${it.value()}\n\n")
                             }
                             samples--
