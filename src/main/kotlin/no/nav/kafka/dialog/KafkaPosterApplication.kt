5e073d82738a70dbfea3ee3c34cd9064448e7af0
Björn Hägglund <bjorn.hagglund@nav.no>
2024-08-07 13:46:11 +0200

    sample offsets log message encoding

--- src/main/kotlin/no/nav/kafka/dialog/KafkaPosterApplication.kt
+++ src/main/kotlin/no/nav/kafka/dialog/KafkaPosterApplication.kt
@@ -33,7 +33,7 @@ class KafkaPosterApplication(
 
     fun start() {
         log.info {
-            "Starting app ${env(config_DEPLOY_APP)} - devContext $devContext" +
+            "Starting app ${env(config_DEPLOY_APP)} - devContext $devContext - messageEncoding ${env(config_MESSAGE_ENCODING)}" +
                 (if (env(config_FLAG_SEEK).toBoolean()) " - SEEK ${env(config_SEEK_OFFSET).toLong()}" else "") +
                 (if (env(config_NUMBER_OF_SAMPLES).toInt() > 0) " - SAMPLE ${env(config_NUMBER_OF_SAMPLES)}" else "") +
                 (if (env(config_FLAG_NO_POST).toBoolean()) " - NO_POST" else "") +
